ce289f7d88c145087fd41f229b087f53ba3b9c01
songshupine <songshupine@live.com>
2026-02-15 16:22:29 +0800

    Create [domain].js

---
+++ functions/udpxy_cmcc/[domain].js
@@ -0,0 +1,70 @@
+
+export async function onRequest(context) {
+    if (!context.params.domain){
+        return new Response(`udpxy参数错误，例：https://iptv.gotonas.com/udpxy_cmcc/192.168.100.1:4022`);
+    }
+    const response = await fetch(`https://iptv.gotonas.com/home/udpxy_cmcc_iptv.m3u8`, {
+        method: 'GET',
+    });
+    let m3uText = await response.text();
+    let url = new URL(context.request.url)
+    if (url.searchParams.get("aptv")) {
+        m3uText = m3uText.replaceAll("{utc:YmdHMS}-{utcend:YmdHMS}", "${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}")
+    }
+
+    m3uText = m3uText.replaceAll("192.168.100.1:4022", context.params.domain)
+
+    const fcc = url.searchParams.get("fcc")
+    if (fcc) {
+        let lines = m3uText.split("\n")
+        lines.forEach(function(line,index){
+            if (line.indexOf("/udp/") > 0) {
+                let url = new URL(line)
+                if (url.searchParams.size > 0){
+                    line += `&fcc=${fcc}`
+                } else {
+                    line += `?fcc=${fcc}`
+                }
+                lines[index] = line;
+            }
+        })
+        m3uText = lines.join("\n")
+    }
+
+    const r2h = url.searchParams.get("r2h-token")
+    if (r2h) {
+        let lines = m3uText.split("\n")
+        lines.forEach(function(line,index){
+            if (line.indexOf("/udp/") > 0) {
+                let url = new URL(line)
+                if (url.searchParams.size > 0){
+                    line += `&r2h-token=${r2h}`
+                } else {
+                    line += `?r2h-token=${r2h}`
+                }
+                lines[index] = line;
+            }
+        })
+        m3uText = lines.join("\n")
+    }
+
+    let rtspProxy = url.searchParams.get("rtspProxy")
+    if (rtspProxy) {
+        if (!rtspProxy.startsWith("http")) {
+            rtspProxy = `http://${rtspProxy}`;
+        }
+        let lines = m3uText.split("\n")
+        lines.forEach(function(line,index){
+            if (line.indexOf("catchup-source=\"rtsp://") > 0) {
+                lines[index] = line.replaceAll("catchup-source=\"rtsp://", `catchup-source="${rtspProxy}/rtsp/`);
+                if (r2h) {
+                    lines[index] = line.replaceAll(/(playseek=[^"&]*)/g,
+                        `$1&r2h-token=${r2h}`)
+                }
+            }
+        })
+        m3uText = lines.join("\n")
+    }
+
+    return new Response(m3uText);
+}
